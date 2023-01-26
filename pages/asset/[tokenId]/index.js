7033088e4e942358a251f2291f6b2c0d29e775ea
unknown <sarahgreyhh@gmail.com>
2023-01-26 11:31:50 +0200

    Update

--- pages/asset/[tokenId]/index.js
+++ pages/asset/[tokenId]/index.js
@@ -8,59 +8,92 @@ import NFTMarketplace from '../../NFTMarketplace.json'
 import AssetNFT from "../../../src/components/Asset"
 
 
-
- 
-  export const fetchNfts = async() => {
-
-    /* create a generic provider and query for unsold market items */
-   // const provider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/ecce808847e94ac0acbf82a064057d47`)
-    const provider = new ethers.providers.JsonRpcProvider()
-    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS, NFTMarketplace.abi, provider)
-    const data = await contract.fetchMarketItems()
-    /*
-    *  map over items returned from smart contract and format 
-    *  them as well as fetch their token metadata
-    * 
-    */
-    const items = await Promise.all(data.map(async i => {
-      const tokenUri = await contract.tokenURI(i.tokenId)
-      
-      const meta = await axios.get(tokenUri)
-      .catch(e => console.error(e))
-      
-     
-      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
-      let item = {
-                    price,
-                    tokenId: i.tokenId.toNumber(),
-                    seller: i.seller,
-                    owner: i.owner,
-                    name: meta.data.name,
-                    description: meta.data.description,
-                    image: meta.data.image,
-      }
-      
-      return item
-  
-    })) 
+export const getStaticPaths = async() => {
+  const provider = new ethers.providers.JsonRpcProvider()
+  const contract = new ethers.Contract(process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS, NFTMarketplace.abi, provider)
+  const data = await contract.fetchMarketItems()
+  /*
+  *  mapping over items returned from smart contract and format 
+  *  them as well as fetch their token metadata
+  * 
+  */
+  const items = await Promise.all(data.map(async i => {
+    const tokenUri = await contract.tokenURI(i.tokenId)
     
-    console.log(items)
+    const meta = await axios.get(tokenUri)
+    .catch(e => console.error(e))
+    
+   
+    let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+    let item = {
+                  price,
+                  tokenId: i.tokenId.toNumber(),
+                  seller: i.seller,
+                  owner: i.owner,
+                  name: meta.data.name,
+                  description: meta.data.description,
+                  image: meta.data.image,
+    }
+    
+    return item
+  })) 
 
-  }
-
-
- const Nft = () => {
   
-  
-  useEffect(() => {
-    fetchNfts()
-  }, [])
-  
+   const paths = items.map(nft => {
+    return{
+      params: { tokenId: nft.tokenId.toString()}
+    }
+   })
 
-
-  return <div>Im individual nft</div>
+   return {
+    paths,
+    fallback:false
+   }
 
 }
+
+export const getStaticProps = async(context) => {
+  const tokenId = context.params.tokenId
+  const provider = new ethers.providers.JsonRpcProvider()
+  const contract = new ethers.Contract(process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS, NFTMarketplace.abi, provider)
+  const data = await contract.fetchMarketItems()
+  /*
+  *  mapping over items returned from smart contract and format 
+  *  them as well as fetch their token metadata
+  * 
+  */
+  const items = await Promise.all(data.map(async i => {
+    const tokenUri = await contract.tokenURI(i.tokenId)
+    
+    const meta = await axios.get(tokenUri)
+    .catch(e => console.error(e))
+    
+   
+    let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+    let item = {
+                  price,
+                  tokenId: i.tokenId.toNumber(),
+                  seller: i.seller,
+                  owner: i.owner,
+                  name: meta.data.name,
+                  description: meta.data.description,
+                  image: meta.data.image,
+    }
+    
+    return item + tokenId
+  })) 
+
+  return {
+    props: {nft: items}
+  }
+
+}
+
+ const Nft = (nft) => {
+
+  return <div>{nft.name}</div>
+}
+
 export default Nft
 
 
